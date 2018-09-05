f0d2074bdeaad2656b6bb36bf00a2a72d2ee2901
mrdurb <mrdurb@users.noreply.github.com>
2018-09-06 00:29:56 +0700

    hi

--- Main/Main/Main.py
+++ Main/Main/Main.py
@@ -1,8 +1,6 @@
 print('Hello GitHub')
 print('Hello DURBEXA')
-<<<<<<< HEAD
 print('VASYA PUPKIN HERE')
-=======
 a = 10
 print('Igor pisa 228')
 a += 1
@@ -11,4 +9,3 @@ if (a == 11)
     
 
 
->>>>>>> e50b9b589e916f34730582a175e06cd43f7709d1
