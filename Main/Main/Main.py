13638b0c5f1834f5ff6fbdeb687c5c206a848b99
igorexa <igor.vlasov2001@gmail.com>
2018-09-06 00:32:18 +0700

    124

--- Main/Main/Main.py
+++ Main/Main/Main.py
@@ -1,6 +1,5 @@
 print('Hello GitHub')
 print('Hello DURBEXA')
-<<<<<<< HEAD
 
 Stas = input ('if u do it - print 1 ')
 if Stas == '1':
@@ -9,14 +8,3 @@ if Stas == '1':
 a = 10
 print('Igor pisa 228')
 a += 1
-=======
-print('VASYA PUPKIN HERE')
-a = 10
-print('Igor pisa 228')
-a += 1
-if (a == 11)
-    a = 9
-    
-
-
->>>>>>> f0d2074bdeaad2656b6bb36bf00a2a72d2ee2901
